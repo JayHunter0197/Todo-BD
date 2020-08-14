e2ae139fa6bc1e7370fc947ae15599bed4e690f2
JayHunter0197 <43304177+JayHunter0197@users.noreply.github.com>
2020-08-13 23:24:24 -0500

    Fix minor changes and the feature of deleting items was implemented

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
 var express = require("express");
 var app = express();
 
-// this is line makes the whole ejs process work
+/**
+ *  ! Without these line ejs won't work
+ */
 app.set("view engine", "ejs");
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ *  ! Without these line our CSS won't work
+ */
 app.use(express.static("public"));
-
 
 const mongoose = require("mongoose");	
 
-// make the connection to the DB 
+/**
+ *  * Create and make the connection to the DB
+ */
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
 
-// how we want the data to be structured in the collection
+
+/**
+ *  * Define the schema of the DB, how we want the data to be structured
+ */
 const itemsSchema = {
 	name: String,
 };
 
-
-// create the collections -- params: name of the DB, the schema of the DB--
+/**
+ *  * Create the collection 
+ *  @param myParams  name of the collection in singular (string), the name of the DB schema
+ */
 const Item = mongoose.model("Item", itemsSchema);
 
+/**
+ *  * Create the Mongoose Documents 
+ *  @param myParam  the schema of the DB (with data)				
+ */
 const item1 = new Item({name: "Buy Food"});
-
-
 const item2 = new Item({name: "Cook Food"});
-//item2.save();
-
 const item3 = new Item({name: "Eat Food"});
-//item3.save();
 
 const tasks = [item1,item2, item3]
 
-// // insert the values in the DB
-//  Item.insertMany(tasks, function (err) {
-//  	if (err) console.log(err);
-//  	else console.log("Data successfully saved into  todolist DB");
-//  });
-
-// // executes, passing results to callback
-// Item.find( {},function (err, docs) {
-// 	if (err) console.log(err);
-// 	else console.log(docs)
-// });
-
+/**
+ *  * Initialize the TodoList DB and display the items 				
+ */
 app.get("/", function(req, res) {
-	
-	// obtain all the data from the DB
+	// Obtain all the data from the DB
 	Item.find( {},function (err, docs) {
-		// if the db is empty insert default values (tasks)
+		// If the db is empty insert default values (tasks)
 		if(docs.length==0)
 		{
-			// insert the initial values for the todo 
 			Item.insertMany(tasks, function (err) {
 				if (err) console.log(err);
 				else console.log("Data successfully saved into  todolist DB");
@@ -65,13 +64,12 @@ app.get("/", function(req, res) {
 		   res.redirect("/");
 		}
 
+		// Display the items in the ToDo list
 		else
 		{
 			res.render("list", { listTitle: "Today", newTask: docs });
 		}	
 	});
-
-
 });
 
 app.get("/about", function(req, res) {
@@ -83,8 +81,11 @@ app.get("/work", function(req, res) {
 	res.render("list", { listTitle: "Work List", newTask: workTasks });
 });
 
+/**
+ * * Add a new item to the ToDo list 
+ */
 app.post("/", function(req, res) {
-	// obtain the item enter by the user
+	// obtain the item enter by the user and save it in the schema
 	 const itenName = req.body.newTask;
 	 const listItem = new Item({name: itenName});
 	 // save the item in the DB
@@ -95,7 +96,18 @@ app.post("/", function(req, res) {
 });
 
 app.post("/delete", function(req,res){
-
+	
+	console.log(req.body.checkbox);
+	const checkedItemId = req.body.checkbox; 
+	
+	Item.findByIdAndRemove(checkedItemId,function(err)
+	{
+		if(!err)
+		{
+			console.log("Item successfully deleted from the DB");
+			res.redirect("/");
+		}
+	}); 
 });
 
 
@@ -105,6 +117,10 @@ app.post("/work", function(req, res) {
 	res.redirect("/work");
 });
 
+
+/**
+ * * Node was runned succesfully
+ */
 app.listen(3000, function() {
 	console.log("Example app listening on port 3000!");
 });
