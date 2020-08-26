15b9b7d41fcc5703ef3f48e5ade460af4fa5c1c4
JayHunter0197 <43304177+JayHunter0197@users.noreply.github.com>
2020-08-26 15:53:12 -0500

    Added new schema and moongose documents, plus now there are dynamic routes

--- app.js
+++ app.js
@@ -32,21 +32,28 @@ const itemsSchema = {
 	name: String,
 };
 
+const listSchema = {
+	name: String,
+	items: [itemsSchema]
+};
+
 /**
  *  * Create the collection 
- *  @param myParams  name of the collection in singular (string), the name of the DB schema
+ *  @param myParams  Name of the collection in singular (string), the name of the DB schema
  */
 const Item = mongoose.model("Item", itemsSchema);
+const List = mongoose.model("List",listSchema);
 
 /**
  *  * Create the Mongoose Documents 
- *  @param myParam  the schema of the DB (with data)				
+ *  @param myParam  The schema of the DB (with data)				
  */
 const item1 = new Item({name: "Buy Food"});
 const item2 = new Item({name: "Cook Food"});
 const item3 = new Item({name: "Eat Food"});
 
-const tasks = [item1,item2, item3]
+const tasks = [item1,item2, item3];
+
 
 /**
  *  * Initialize the TodoList DB and display the items 				
@@ -63,7 +70,6 @@ app.get("/", function(req, res) {
 			});
 		   res.redirect("/");
 		}
-
 		// Display the items in the ToDo list
 		else
 		{
@@ -72,29 +78,58 @@ app.get("/", function(req, res) {
 	});
 });
 
+
+
 app.get("/about", function(req, res) {
 	res.render("about");
 });
 
+/**
+ * *Create personalized lists by creating dynamic routes 
+ * @params name of the dynamic route, callback function
+ */
+app.get("/:customListName", function(req,res){
+	// Get the name of the list from the url
+	const customListName = req.params.customListName;
+	
+	// Look if a list with the same name already exits
+	List.findOne({name: customListName},function(err, foundList){
+		if(!err)
+		{
+			// If a list was not find add it to the DB
+			if(!foundList)
+			{
+				const list = new List({
+					name: customListName,
+					items: tasks
+				});
+				list.save();
+				res.redirect("/"+customListName);
+			}
 
-app.get("/work", function(req, res) {
-	res.render("list", { listTitle: "Work List", newTask: workTasks });
+			else{
+				res.render("list", { listTitle: foundList.name, newTask: foundList.items });
+			}
+		}
+	})
+
 });
 
 /**
  * * Add a new item to the ToDo list 
  */
 app.post("/", function(req, res) {
-	// obtain the item enter by the user and save it in the schema
+	// Obtain the item enter by the user and save it in the schema
 	 const itenName = req.body.newTask;
-	 const listItem = new Item({name: itenName});
-	 // save the item in the DB
+	 const listItem = new Item({name: itenName}); 
 	 listItem.save();
 	 res.redirect("/");
 	 
 	
 });
-
+/**
+ * * Delete an item from the ToDo list 
+ */
 app.post("/delete", function(req,res){
 	
 	console.log(req.body.checkbox);
